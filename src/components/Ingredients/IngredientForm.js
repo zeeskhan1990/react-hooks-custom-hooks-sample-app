02aba311c929883a174264e383d6925bdc64eb2d
Zeeshan Khan <zeeskhan1990@gmail.com>
2019-09-16 11:27:31 +0530

    Composite state

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+  /**
+   * It takes a default state as the first arg with which it is initialized. This state can be anything
+   * whereas in class based state it needed to be an object as a must.
+   */
+  const [inputState, setInputState] = useState({title: '', amount: ''})
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -15,11 +20,27 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input type="text" id="title" value={inputState.title}
+              onChange={ev => {
+                    //ev.persist()
+                    const newTitle = ev.target.value
+                    setInputState((prevInputState) => {
+                      return {title: newTitle, amount: prevInputState.amount}
+                    })
+                  }
+                }/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount" value={inputState.amount}
+                  onChange={ev => {
+                    //ev.persist()
+                    const newAmount = ev.target.value
+                    setInputState((prevInputState) => {
+                      return {title: prevInputState.title, amount: newAmount}
+                    })
+                  }
+                }/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
